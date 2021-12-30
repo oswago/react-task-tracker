08a5db339fbfc06e7e8b15c2205d064c1608c088
Elly Oswago [ICT] <eoswago@CO-OPBANK.CO.KE>
2021-12-30 13:30:51 +0300

    Server Data Fetching

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
+import  Header from './componenets/Header'
+import Tasks from './componenets/Tasks'
+import {useState,useEffect} from 'react';
+import AddTasks from './componenets/AddTasks';
 function App() {
+  const[showAddTask,setShowAddtask]=useState(false);
+  const[tasks,setTasks]=useState([]);
+   
+  useEffect(()=>{
+     const getTasks=async()=>{
+           const taskFromServer=await fetchTasks()
+            setTasks(taskFromServer)
+     }
+    getTasks()
+  },[]);
+
+  //Fetch data from server
+  const fetchTasks=async()=>{
+      const res=await fetch('http://localhost:5000/tasks')
+       const data=await res.json();
+
+      return data;
+  }
+
+  //Add task
+    const addTask=(taskspassed)=>{
+       const id=Math.floor(Math.random()*1000)+1;
+       const newTask={id, ...taskspassed };
+       setTasks([...tasks,newTask]);
+     // console.log(newTask);
+    };
+
+  //delete  Task
+   const delTask=(id)=>{
+    setTasks(tasks.filter((task)=>task.id!==id));
+   };
+ 
+   const toggleReminder=(id)=>{
+     setTasks(tasks.map((task)=>task.id==id ? {...task,reminder:!task.reminder}:task));
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title='Tasks Tracker' 
+        onAdd={()=>setShowAddtask(!showAddTask)}
+        showAdd={showAddTask}
+       />
+      {showAddTask && <AddTasks onAdd={addTask}/> }
+      {tasks.length>0 ?<Tasks tasks={tasks} onDelete={delTask} onToggle={toggleReminder} />:"No More Tasks"}
     </div>
   );
 }
