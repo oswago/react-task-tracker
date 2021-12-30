caa26f5e1da1b27b8861987b0af8f60c895e5e7a
Elly Oswago [ICT] <eoswago@CO-OPBANK.CO.KE>
2021-12-30 17:32:58 +0300

    Routes and links

--- src/componenets/About.js
+++ src/componenets/About.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
     return (
         <div>
             <h4>This is about Page</h4>
-            <a href='/'>Go Back</a>
+            <Link to='/'>Go Back</Link>
         </div>
     )
 }
